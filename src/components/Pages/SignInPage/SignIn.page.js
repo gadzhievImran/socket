a4fe60bd790eabc14bcdbf30d67a22986af00963
Ima Gadzhiev <sdamegeoge@gmail.com>
2019-01-11 02:52:55 +0300

    update

--- src/components/Pages/SignInPage/SignIn.page.js
+++ src/components/Pages/SignInPage/SignIn.page.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Cookies } from 'react-cookie';
+import axios from 'axios';
+
 import '../../../assets/css/pages/signin.sass';
 import {PAGE_MAIN} from "../../../config/links";
 
@@ -7,8 +9,8 @@ export default class SignInPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: null,
-            password: null
+            password: null,
+            name: null
         };
     }
 
@@ -26,8 +28,29 @@ export default class SignInPage extends Component {
         // I have to make request to take token and inline with status I have to continue
         event.preventDefault();
         const { cookies } = this.props;
-        cookies.set('token', '1234567890qwertyuiop');
-        this.props.history.push(PAGE_MAIN)
+        const { name, password } = this.state;
+        // fetch('/authentication', {
+        //     method: 'post',
+        //     headers: {'Content-Type':'application/json'},
+        //     body: JSON.stringify({
+        //         name,
+        //         password
+        //     })
+        // }).then(res => res.json()).then(res => {
+        //     console.log('res', res);
+        // });
+
+        axios.post('http://localhost:3000/authentication', {
+            name,
+            password
+        }).then(response => {
+            this.setState({ token: response.data.payload.token }, () => {
+                cookies.set('token', this.state.token);
+                this.props.history.push(PAGE_MAIN)
+            })
+        }).catch(error => {
+            console.error('Enter correct your password or name.', error);
+        });
     };
 
     render() {
@@ -36,13 +59,13 @@ export default class SignInPage extends Component {
                 <h1>Sign in</h1>
                 <form>
                     <div className="wrapper-item">
-                        <div className="title">Email</div>
+                        <div className="title">Name</div>
                         <input
                             className="input__text"
                             type="text"
-                            name="email"
+                            name="name"
                             onChange={this.handleChangeUserData}
-                            placeholder="email"
+                            placeholder="name"
                         />
                     </div>
                     <div className="wrapper-item">
